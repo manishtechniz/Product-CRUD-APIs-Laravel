45e8b8bcc6b6f0e45401fa307b8c8766b340ae68
manishtechniz <manishtechniz@gmail.com>
2025-03-14 09:59:03 +0530

    Create Product and Product Image Factory Data

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class ProductImage extends Model
 {
+    use HasFactory;
+    
     /**
      * Summary of timestamps
      */
---
+++ database/factories/ProductImageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\ProductImage;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductImage>
+ */
+class ProductImageFactory extends Factory
+{
+    protected $model =  ProductImage::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'path'       => fake()->imageUrl(),
+            'product_id' => Product::factory(),
+        ];
+    }
+}
